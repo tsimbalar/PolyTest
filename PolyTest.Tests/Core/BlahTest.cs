af0523ad0c19884ae706b63b68b44ede120bf07f
Thibaud Desodt <tibo.desodt@gmail.com>
2012-11-30 07:32:20 +0100

    basic nested test cases working

--- PolyTest.Tests/Core/BlahTest.cs
+++ PolyTest.Tests/Core/BlahTest.cs
@@ -24,21 +24,62 @@ namespace PolyTest.Tests.Core
             IInitialStateCollection<DummyItem> testCases = new InitialStateCollection<DummyItem>();
             testCases.Add(new SequentialMutations<DummyItem>(initialCondition, new Mutation<DummyItem>("setting it to 4", d => { d.IntProperty = 4; })));
             testCases.Add(new SequentialMutations<DummyItem>(initialCondition, new Mutation<DummyItem>("setting it to 3", d => { d.IntProperty = 3; })));
-            testCases.Add(new SequentialMutations<DummyItem>(initialCondition,
-                new Mutation<DummyItem>("adding 2", d => { d.IntProperty = d.IntProperty + 2; }),
-                new Mutation<DummyItem>("removing 1", d => { d.IntProperty = d.IntProperty - 1; })));
+
+            var adding2 = new SequentialMutations<DummyItem>(initialCondition, new Mutation<DummyItem>("adding 2", d => { d.IntProperty = d.IntProperty + 2; }));
+            testCases.Add(new SequentialMutations<DummyItem>(adding2, new Mutation<DummyItem>("removing 1", d => { d.IntProperty = d.IntProperty - 1; })));
+            testCases.Add(new SequentialMutations<DummyItem>(adding2, new Mutation<DummyItem>("removing 3", d => { d.IntProperty = d.IntProperty - 3; })));
+
 
 
             var results = testCases.Execute(it => sut.DoIt(it));
 
 
             results.ForEach((str, val) => AssertIsNotFive(val, str));
-            
+
 
             // Act
 
             // Assert
 
+        }
+
+        [TestMethod]
+        public void FluentFlatTest()
+        {
+            // Arrange
+            var sut = new FakeSut();
+            "Starting with IntProperty = 5".AsStartingPoint(() => new DummyItem(5))
+                .Arrange("setting it to 4", d => { d.IntProperty = 4; })
+                .Arrange("setting it to 3", d => { d.IntProperty = 3; })
+            .Act(it => sut.DoIt(it))
+            .Assert((str, val) => AssertIsNotFive(val, str));
+
+            // Act
+
+            // Assert
+
+        }
+
+        [TestMethod]
+        public void FluentNestedTest()
+        {
+            // Arrange
+            var sut = new FakeSut();
+            "Starting with IntProperty = 5".AsStartingPoint(() => new DummyItem(5))
+                .Arrange("setting it to 4", d => { d.IntProperty = 4; }, 
+                    andThen => andThen.IgnoringRoot()
+                        .With(new Mutation<DummyItem>("setting bool to false", d => { d.BoolProperty = false; }))
+                        .With(new Mutation<DummyItem>("setting bool to true", d => { d.BoolProperty = true; }))
+                )
+                .Arrange("setting it to 3", d => { d.IntProperty = 3; })
+             // Act
+            .Act(it =>
+                     {
+                         sut.HasIt(it);
+                         return it;
+                     })
+            // Assert
+            .Assert((str, val) => Assert.AreEqual(true, val.BoolProperty, str));
         }
 
 
@@ -50,10 +91,12 @@ namespace PolyTest.Tests.Core
             public DummyItem(int initial)
             {
                 IntProperty = initial;
+                BoolProperty = false;
             }
 
             public int IntProperty { get; set; }
 
+            public bool BoolProperty { get; set; }
 
         }
 
@@ -62,6 +105,11 @@ namespace PolyTest.Tests.Core
             public int DoIt(DummyItem item)
             {
                 return item.IntProperty;
+            }
+
+            public bool HasIt(DummyItem item)
+            {
+                return item.BoolProperty;
             }
         }
 
@@ -74,6 +122,144 @@ namespace PolyTest.Tests.Core
         }
 
         #endregion
+    }
+
+    public static class PolyTestExtensions
+    {
+        public static IInitialStateCollectionFromStartingPoint<T> AsStartingPoint<T>(this string description, Func<T> initializationMethod)
+        {
+            // TODO : invent description with reflection ?
+            var start =  new StartingPoint<T>(description, initializationMethod);
+            return new InitialStateCollectionFromStartingPoint<T>(start);
+        }
+
+
+        public static IInitialStateCollectionFromStartingPoint<T> Arrange<T>(this IInitialStateCollectionFromStartingPoint<T> collection,
+                                                           string mutationDescription, Action<T> modification)
+        {
+            collection.Add(new Mutation<T>(mutationDescription, modification));
+            return collection;
+        }
+
+        public static IInitialStateCollectionFromStartingPoint<T> Arrange<T>(this IInitialStateCollectionFromStartingPoint<T> collection,
+                                                   string mutationDescription, Action<T> modification,
+                                                    Func<MutationList<T>, MutationList<T>> nested )
+        {
+            var commonMutationToApplyToAllNested = new Mutation<T>(mutationDescription, modification);
+
+            var populatedNested = nested(new MutationList<T>());
+            foreach (var mutation in populatedNested.Compose(commonMutationToApplyToAllNested))
+            {
+                // add all the nested mutations to the global list
+                collection.Add(mutation);
+            }
+            return collection;
+        } 
+
+        public static IIndividualTestExecutionInformationCollection<TResult> Act<T, TResult>(
+            this IInitialStateCollectionFromStartingPoint<T> startingPoints,
+            Func<T, TResult> testedMethod)
+        {
+            return startingPoints.Execute(testedMethod);
+        }
+
+        public static void Assert<TResult>(
+    this IIndividualTestExecutionInformationCollection<TResult> result,
+    Action<string, TResult> assertion)
+        {
+            result.ForEach(assertion);
+        }
+    }
+
+    public class MutationList<T>
+    {
+        private readonly List<IMutation<T>> _mutations;
+        private bool _shouldConsiderParentAsACondition;
+
+        public MutationList()
+        {
+            _mutations = new List<IMutation<T>>();
+            _shouldConsiderParentAsACondition = true; // considerMe a condition
+        }
+
+        public MutationList<T> With(IMutation<T> mutationToAdd)
+        {
+            _mutations.Add(mutationToAdd);
+            return this;
+        } 
+
+        public IEnumerable<IMutation<T>> Compose(IMutation<T> root)
+        {
+            if (_shouldConsiderParentAsACondition)
+                yield return new Mutation<T>(root.Description + " (first before nested conditions)", (a) => root.Apply(a));
+            foreach (var mutation in _mutations)
+            {
+                // add all the nested mutations to the global list
+                var composedMutation = new CompositeMutation<T>(root, mutation);
+                yield return composedMutation;
+            }
+        }
+
+        public MutationList<T> IgnoringRoot(bool shouldIgnore = true)
+        {
+            this._shouldConsiderParentAsACondition = !shouldIgnore;
+            return this;
+        }
+    }
+
+    public class CompositeMutation<T> : IMutation<T>
+    {
+        private readonly IMutation<T> _root;
+        private readonly IMutation<T> _second;
+
+        public CompositeMutation(IMutation<T> root, IMutation<T> second)
+        {
+            _root = root;
+            _second = second;
+        }
+
+        public string Description { get { return _root.Description + " AND " + _second.Description; } }
+        public void Apply(T source)
+        {
+            _root.Apply(source);
+            _second.Apply(source);
+        }
+    }
+
+    public interface IStateCollection<T>
+    {
+        IIndividualTestExecutionInformationCollection<TResult> Execute<TResult>(Func<T, TResult> testMethod);
+    }
+
+    public interface IInitialStateCollectionFromStartingPoint<T> : IStateCollection<T>
+    {
+        IInitialization<T> StartingPoint { get; } 
+        void Add(IMutation<T> mutation);
+        
+    }
+
+    public class InitialStateCollectionFromStartingPoint<T> : IInitialStateCollectionFromStartingPoint<T>
+    {
+        private readonly IInitialization<T> _startingPoint;
+        private readonly InitialStateCollection<T> _internalCollection;
+
+        public InitialStateCollectionFromStartingPoint(IInitialization<T> startingPoint)
+        {
+            _startingPoint = startingPoint;
+            _internalCollection = new InitialStateCollection<T>();
+        }
+
+        public IInitialization<T> StartingPoint { get { return _startingPoint; } }
+
+        public void Add(IMutation<T> mutation)
+        {
+            _internalCollection.Add(new SequentialMutations<T>(_startingPoint, mutation));
+        }
+
+        public IIndividualTestExecutionInformationCollection<TResult> Execute<TResult>(Func<T, TResult> testMethod)
+        {
+            return _internalCollection.Execute(testMethod);
+        }
     }
 
 
@@ -131,6 +317,7 @@ namespace PolyTest.Tests.Core
                 return sb.ToString();
             }
         }
+
     }
 
     /// <summary>
@@ -160,13 +347,9 @@ namespace PolyTest.Tests.Core
     }
 
 
-
-    
-
-    internal interface IInitialStateCollection<T>
+    public interface IInitialStateCollection<T> : IStateCollection<T>
     {
         void Add(IInitialization<T> state);
-        IIndividualTestExecutionInformationCollection<TResult> Execute<TResult>(Func<T, TResult> testMethod);
     }
 
     internal class InitialStateCollection<T> : IInitialStateCollection<T>
@@ -192,7 +375,7 @@ namespace PolyTest.Tests.Core
                         (initial, idx) =>
                         new IndividualTestExecutionInformation<TResult>(idx, initial.Description,
                                                                         testMethod(initial.Prepare()))));
-        }  
+        }
     }
 
     internal class IndividualTestExecutionInformationCollection<TResult> : IIndividualTestExecutionInformationCollection<TResult>
@@ -222,7 +405,7 @@ namespace PolyTest.Tests.Core
     public interface IIndividualTestExecutionInformation<TResult>
     {
 
-        int Index { get;}
+        int Index { get; }
         string TestCaseDescription { get; }
         TResult Result { get; }
 
