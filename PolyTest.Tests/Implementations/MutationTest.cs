d886319670faf2f3ba1738f9ec4a6d256b9f01ba
Thibaud Desodt <tibo.desodt@gmail.com>
2013-03-12 07:42:51 +0100

    Added unit tests for Mutation class

---
+++ PolyTest.Tests/Implementations/MutationTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PolyTest.Implementations;
+using PolyTest.Tests.TestUtils;
+using Xunit;
+
+namespace PolyTest.Tests.Implementations
+{
+    public class MutationTest
+    {
+        [Fact]
+        public void Ctor_with_mutationDescription_null_throws_ArgumentNullException()
+        {
+            // Arrange
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new Mutation<DummyItem>(null, d => d.IntProperty = 2)
+            );
+        }
+
+        [Fact]
+        public void Ctor_with_mutationToApply_null_throws_ArgumentNullException()
+        {
+            // Arrange
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new Mutation<DummyItem>("someString", null)
+            );
+        }
+
+        [Fact]
+        public void Ctor_assigns_property_Description()
+        {
+            // Arrange
+            var someString = "any string";
+            
+            // Act
+            var sut = new Mutation<DummyItem>(someString, d => d.IntProperty = 2);
+
+            // Assert
+            Assert.Equal(someString, sut.Description);
+        }
+
+        [Fact]
+        public void Apply_calls_mutationToApply_on_argument()
+        {
+            // Arrange
+            var initialValue = 4;
+            var initial = new DummyItem(initial: initialValue, initialBool: false);
+            var sut = new Mutation<DummyItem>("some string", d => d.IntProperty += 2);
+
+            // Act
+           sut.Apply(initial);
+
+            // Assert
+            Assert.Equal(initialValue + 2, initial.IntProperty);
+        }
+
+    }
+}
